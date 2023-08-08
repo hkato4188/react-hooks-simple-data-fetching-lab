3fcb5beaa526742c68dec14fb22cdb3f00fe5976
Hiroki Kato <hirokikato1@gmail.com>
2023-08-07 23:19:17 -0400

    create App component to demonstrate get fetch request and conditional rendering

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
-// create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [image, setImage] = useState("");
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    fetch(`https://dog.ceo/api/breeds/image/random`)
+      .then((res) => res.json())
+      .then((data) => {
+        setImage(() => data.message);
+        setIsLoaded(() => !isLoaded);
+      });
+  }, []);
+
+  return (
+    <div>
+      {!isLoaded ? (
+        <h1>...Loading...</h1>
+      ) : (
+        <img src={image} alt="A Random Dog" />
+      )}
+    </div>
+  );
+}
+
+export default App;
